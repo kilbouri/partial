311f6ebd554c0a70947309bf33e2c5a9655b1b06
DavidBaillie <DavidlBaillie@gmail.com>
2026-02-16 12:04:12 -0500

    working on tests

---
+++ Partial.SystemTextJson.Tests/PartialTests.cs
@@ -0,0 +1,38 @@
+﻿using Partial.Core;
+using Shouldly;
+using System.Text.Json;
+
+namespace Partial.SystemTextJson.Tests;
+
+file class TestUser : Partial<TestUser>
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; } = -1;
+    public double BankBalance { get; set; } = 0f;
+}
+
+[TestFixture]
+public class PartialTests
+{
+    [Test]
+    public void PartialModelSerializesAllProperties()
+    {
+        // Arrange
+        var inboundJson = """
+            {
+                "Name": "John Doe",
+                "Age": 15,
+                "BankBalance": 125.25
+            }
+            """;
+
+        // Act
+        var model = JsonSerializer.Deserialize<TestUser>(inboundJson);
+
+        // Assert
+        model.ShouldNotBeNull();
+        model.IsDefined(x => x.BankBalance).ShouldBeTrue();
+        model.IsDefined(x => x.Age).ShouldBeTrue();
+        model.IsDefined(x => x.Name).ShouldBeTrue();
+    }
+}
